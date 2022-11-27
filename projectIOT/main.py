d50c11184f8ea464938a8612c3f7efcf034bb1ec
esquigor <esquivelguillerm@gmail.com>
2022-11-26 22:53:56 -0600

    V2.5

--- projectIOT/main.py
+++ projectIOT/main.py
@@ -1,118 +1,100 @@
-#import Rpi.GPIO as GPIO # Puertos raspberry 
+import RPi.GPIO as GPIO # Puertos raspberry 
 #from picamera import PiCamera
-# from time import sleep
 import methodsIOT as iot
-import sqlite3
-from datetime import datetime
-import mysql.connector
+#import sqlite3
+#from datetime import datetime
+#import mysql.connector
 
-# General Configuration
-#GPIO.setwarnings(False)
+# -_-_-_-_-_-_-_-_-_-_-_- General Configuration -_-_-_-_-_-_-_-_-_-_-_-
 
-# -_-_-_-_-_-_-_-_-_-_-_- Connection to the database -_-_-_-_-_-_-_-_-_-_-_
+# --- Sensors ---
+GPIO.setmode(GPIO.BCM) # Nomenclatura NO fisica
+GPIO.setwarnings(False)
+GPIO.setup(16, GPIO.OUT) # Led blanco
+GPIO.setup(26, GPIO.OUT) # Led Rojo
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Boton
 
-db = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="login"
-)
-crsr = db.cursor() 
+# --- Variables ---
+n = 2 # Leds time 
+data = "estudiante" # 1st choice
+matAlu = "01625621" # 2nd choice
 
-print(db) 
-# -_-_-_-_-_-_-_-_-_-_-_-  Main -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+## -_-_-_-_-_-_-_-_-_-_-_- Connection to the database -_-_-_-_-_-_-_-_-_-_-_
+#
+#db = mysql.connector.connect(
+#  host="localhost",
+#  user="root",
+#  password="",
+#  database="login"
+#)
+#crsr = db.cursor() 
+#
+#print(db) 
+## -_-_-_-_-_-_-_-_-_-_-_-  Main -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
-if GPIO.input(10) == GPIO.HIGH:
-    print("Inicia proceso")
+while True:
+    if GPIO.input(15) == GPIO.HIGH:
+        print("Inicia proceso")
+    #    iot.t2s("Bienvenido al Tecnologico de Monterrey, ¿es usted estudiante o colaborador?")
+    #    #data = iot.recordAudio(3)
 
-    iot.t2s("Bienvenido al Tecnologico de Monterrey, ¿es usted estudiante o colaborador?")
-    #data = iot.recordAudio(3)
-    data = "estudiante"
+        i = True
+        while i == True:
+            # -_-_-_-_-_-_-_-_-_-_-_-  Casos posibles, estudiante/colaborador/externo  -_-_-_-_-_-_-_-_-_-_-_-      
+            if ("estudiante") in data:
+    #            #iot.t2s("Dígame su matrícula sin la primer letra") #01 625 621
+                print("Digame matricula...")
+    #            #matAlu = iot.recordAudio(5)
+    #            #matAlu = matAlu.replace(' ', '').replace(',','').replace('.','').replace('-','') # Limpieza de la data
+                try:
+                    if(len(matAlu) == 8):
+    #                    matAlu = "A" + matAlu
+    #                    query = "SELECT `ID_Usuario` FROM `usuarios`;"
+    #                    
+    #                    # -_-_-_-_-_-_-_-_-_-_-_-  Obtenemos las matriculas en la database -_-_-_-_-_-_-_-_-_-_-_-
+    #                    crsr.execute(query)
+    #                    matriculas = crsr.fetchall()
+    #                    # matriculas.append(str(row).replace(',','').replace("'",'').replace('(','').replace(')',''))
+    #
+    #                    # -_-_-_-_-_-_-_-_-_-_-_-  Buscamos si alguna coincide -_-_-_-_-_-_-_-_-_-_-_-
+    #                    for i in range(len(matriculas)):
+    #                        if str(matriculas[i]) == matAlu:
+    #                            #iot.t2s("Muchas gracias, que tenga buen dia!")
+    #                            print(f"Texto 1 = {matAlu}")
+    #                            i = False
 
-    i = True
-    while i == True:
-        # -_-_-_-_-_-_-_-_-_-_-_-  Casos posibles, estudiante/colaborador/externo  -_-_-_-_-_-_-_-_-_-_-_-      
-        if ("estudiante") in data:
-            #iot.t2s("Dígame su matrícula sin la primer letra") #01 625 621
-            print("Digame matricula...")
-            #matAlu = iot.recordAudio(5)
-            #matAlu = matAlu.replace(' ', '').replace(',','').replace('.','').replace('-','') # Limpieza de la data
-            try:
-                matAlu = "01625621" # test
-                if(len(matAlu) == 8):
-                    matAlu = "A" + matAlu
-                    query = "SELECT `ID_Usuario` FROM `usuarios`;"
-                    
-                    # -_-_-_-_-_-_-_-_-_-_-_-  Obtenemos las matriculas en la database -_-_-_-_-_-_-_-_-_-_-_-
-                    crsr.execute(query)
-                    matriculas = crsr.fetchall()
-                    # matriculas.append(str(row).replace(',','').replace("'",'').replace('(','').replace(')',''))
+                                # -_-_-_-_-_-_-_-_-_-_- Damos acceso -_-_-_-_-_-_-_-_-_-_-                                    
+                                #iot.t2s(f"Acceso aprovado, se mantendra abierto por {n}")
+                                iot.ledOn(16, n)
+                                # -_-_-_-_-_-_-_-_-_- Foto de seguridad -_-_-_-_-_-_-_-_-_-_- 
 
-                    # -_-_-_-_-_-_-_-_-_-_-_-  Buscamos si alguna coincide -_-_-_-_-_-_-_-_-_-_-_-
-                    for i in range(len(matriculas)):
-                        if str(matriculas[i]) == matAlu:
-                            #iot.t2s("Muchas gracias, que tenga buen dia!")
-                            print(f"Texto 1 = {matAlu}")
-                            i = False
-                            #iot.t2s("Se supone que ya deben abrir las puertas en este punto, Viva Mexico siiiuuuuuuuu")
-
-                            # -_-_-_-_-_-_-_-_-_-_-  Insertamos la hora de acceso
-                            date_time = datetime.now().strftime("%m/%d/%Y_%H:%M")
-
-                            #querySQL = "INSERT INTO `access` (`id_Access`, `time`, `matricula_id`) values ('','"+ date_time +"','"+ matAlu +"');"
-                            querySQL = "INSERT INTO `access` (`id_Access`, `time`, `matricula_id`) values ('1','holaa','A01625621');"
-                            crsr.execute(querySQL)
-                            print("yeap")
-                            # Procede a abrir las puertas, encendemos led
-                    if (i == 1):
-                        iot.t2s("Matricula inválida")
-                        print(f"Texto = {matAlu}")
-                        i = False
-                        # Procede a no dar el paso, encender 2do led
-                else:
-                    iot.t2s("Hubo un error, por favor")    
-                    print(f"Texto 1= {matAlu}")
-            except:
-                iot.t2s("Hubo un error, por favor")
-                print(f"Texto 2= {matAlu}")
-        elif ("colaborador") in data:
-            iot.t2s("Dígame su nomina sin la primer letra") #01 625 621
-            matCol = iot.recordAudio(5)
-            matCol = matCol.replace(' ', '').replace(',','').replace('.','').replace('-','') # Limpieza de la data
-            try:
-                #matCol = "01625621" # test
-                if(len(matCol) == 8):
-                    matCol = "L" + matCol
-                    querySQL = "SELECT `matricula` FROM `users`;"
-                    matriculas = []
-                    # -_-_-_-_-_-_-_-_-_-_-_-  Obtenemos las matriculas en la database -_-_-_-_-_-_-_-_-_-_-_-
-                    for i, row in enumerate(crsr.execute(querySQL)):
-                        matriculas.append(str(row).replace(',','').replace("'",'').replace('(','').replace(')',''))
-
-                    # -_-_-_-_-_-_-_-_-_-_-_-  Buscamos si alguna coincide -_-_-_-_-_-_-_-_-_-_-_-
-                    for i in range(len(matriculas)):
-                        if str(matriculas[i]) == matCol:
-                            iot.t2s("Muchas gracias, que tenga buen dia!")
-                            print(f"Texto 1 = {matCol}")
-                            i = False
-                            # Procede a abrir las puertas
-                    if (i == 1):
-                        iot.t2s("Nomina inválida")
-                        print(f"Texto = {matCol}")
-                        i = False
-                        # Procede a no dar el paso
-                else:
-                    iot.t2s("Hubo un error, por favor")    
-                    print(f"Texto 1= {matCol}")
-            except:
-                iot.t2s("Hubo un error, por favor")
-                print(f"Texto 2= {matCol}")
-        elif () in data:
-            iot.t2s("Una disculpa, no es posible que usted ingrese por este lugar, favor de retornar y entrar por la entrada principal.")
-            i = False
-        else:
-            iot.t2s("No se pudo entender su respuesta, por favor repita.")
-            data = iot.recordAudio(3) # Recibir audio
-
-
-    
+    #                            date_time = datetime.now().strftime("%m/%d/%Y_%H:%M")
+    #
+                                # -_-_-_-_-_-_-_-_-_-  Registro de acceso -_-_-_-_-_-_-_-_-_-_- 
+    #                            querySQL = "INSERT INTO `access` (`id_Access`, `time`, `matricula_id`) values ('','"+ date_time +"','"+ matAlu +"');"
+    #                            querySQL = "INSERT INTO `access` (`id_Access`, `time`, `matricula_id`) values ('1','holaa','A01625621');"
+    #                            crsr.execute(querySQL)
+                                print("Proceso estudiante finalizado")
+    #                    if (i == 1):
+    #                        iot.t2s("Matricula inválida")
+    #                        print(f"Texto = {matAlu}")
+    #                        i = False
+                            # Procede a no dar el paso, encender 2do led
+                            iot.ledOn(26, n)
+                    else:
+    #                    iot.t2s("Hubo un error, por favor")    
+                        print(f"Texto 1= {matAlu}")
+                        iot.ledOn(26, n)
+                except:
+    #                iot.t2s("Hubo un error, por favor")
+    #                print(f"Texto 2= {matAlu}")
+                    iot.ledOn(26, n)
+            elif ("colaborador") in data:
+                print("Camino colaborador")
+            elif ("externo") in data:
+    #            iot.t2s("Una disculpa, no es posible que usted ingrese por este lugar, favor de retornar y entrar por la entrada principal.")
+                i = False
+                iot.ledOn(26, n)
+            else:
+    #            iot.t2s("No se pudo entender su respuesta, por favor repita.")
+    #            data = iot.recordAudio(3) # Recibir audio
--- projectIOT/methodsIOT.py
+++ projectIOT/methodsIOT.py
@@ -1,78 +1,84 @@
+import RPi.GPIO as GPIO
+from time import sleep
 #import whisper # Speech2text model from OpenAI
-import pyaudio # Record audio
-import wave # Record audio
-from gtts import gTTS # Text2speech
-import pygame # Run sound
-from picamera import PiCamera
+#import pyaudio # Record audio
+#import wave # Record audio
+#from gtts import gTTS # Text2speech
+#import pygame # Run sound
+#from picamera import PiCamera
+#
+## General configuration
+##model = whisper.load_model("base")
+#
+## Camera configuration
+#camera = PiCamera()
+#camera.resolution = (640, 480)
+#camera.framerate = 15
+#
+## -_-_-_-_-_-_-_-_-_-_-_-_ Method to get the audio -_-_-_-_-_-_-_-_-_-_-_-_
+#def getAudio(time):
+#    FRAME_PER_BUFFER = 3200
+#    FORMAT = pyaudio.paInt16 
+#    CHANNELS = 1 # Monoformat
+#    RATE = 16000
+#    
+#    p = pyaudio.PyAudio()
+#    
+#    stream = p.open(
+#        format = FORMAT,
+#        channels = CHANNELS,
+#        rate = RATE,
+#        input = True,
+#        frames_per_buffer = FRAME_PER_BUFFER
+#    )
+#    
+#    print(f"Grabando por {time} segundos")
+#    
+#    sec = time
+#    frames = []
+#    for i in range(0, int(RATE/FRAME_PER_BUFFER*sec)):
+#        data = stream.read(FRAME_PER_BUFFER)
+#        frames.append(data)
+#    
+#    stream.close()
+#    p.terminate()
+#    
+#    obj =  wave.open("output.wav","wb")
+#    obj.setnchannels(CHANNELS)
+#    obj.setsampwidth(p.get_sample_size(FORMAT))
+#    obj.setframerate(RATE)
+#    obj.writeframes(b"".join(frames))
+#    obj.close()
+#
+## -_-_-_-_-_-_-_-_-_-_-_-_ Speech to text method -_-_-_-_-_-_-_-_-_-_-_-_
+#def s2t():
+#    result = model.transcribe("output.wav", fp16=False, language='Spanish')    
+#    return(result["text"].lower())
+#
+## -_-_-_-_-_-_-_-_-_-_-_-_ Text to speech method -_-_-_-_-_-_-_-_-_-_-_-_
+#def t2s(msg):
+#    speech = gTTS(text = msg, lang = 'es')
+#    speech.save('computerAudio.mp3')
+#    pygame.mixer.init()
+#    pygame.mixer.music.load("computerAudio.mp3")
+#    pygame.mixer.music.play()
+#    while pygame.mixer.music.get_busy() == True:
+#        continue
+#
+## -_-_-_-_-_-_-_-_-_-_-_-_ Get audio and apply s2t -_-_-_-_-_-_-_-_-_-_-_-_     
+#def recordAudio(time):
+#    getAudio(time)
+#    data = s2t()
+#    return data
+#
+## -_-_-_-_-_-_-_-_-_-_-_-_  -_-_-_-_-_-_-_-_-_-_-_-_
+#def capture_photo(file_capture, text):
+#    camera.annotate_text = text
+#    sleep(2)
+#    camera.capture(file_capture)
+#    print("\r\nImage Captured! \r\n")
 
-# General configuration
-#model = whisper.load_model("base")
-
-# Camera configuration
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 15
-
-# -_-_-_-_-_-_-_-_-_-_-_-_ Method to get the audio -_-_-_-_-_-_-_-_-_-_-_-_
-def getAudio(time):
-    FRAME_PER_BUFFER = 3200
-    FORMAT = pyaudio.paInt16 
-    CHANNELS = 1 # Monoformat
-    RATE = 16000
-    
-    p = pyaudio.PyAudio()
-    
-    stream = p.open(
-        format = FORMAT,
-        channels = CHANNELS,
-        rate = RATE,
-        input = True,
-        frames_per_buffer = FRAME_PER_BUFFER
-    )
-    
-    print(f"Grabando por {time} segundos")
-    
-    sec = time
-    frames = []
-    for i in range(0, int(RATE/FRAME_PER_BUFFER*sec)):
-        data = stream.read(FRAME_PER_BUFFER)
-        frames.append(data)
-    
-    stream.close()
-    p.terminate()
-    
-    obj =  wave.open("output.wav","wb")
-    obj.setnchannels(CHANNELS)
-    obj.setsampwidth(p.get_sample_size(FORMAT))
-    obj.setframerate(RATE)
-    obj.writeframes(b"".join(frames))
-    obj.close()
-
-# -_-_-_-_-_-_-_-_-_-_-_-_ Speech to text method -_-_-_-_-_-_-_-_-_-_-_-_
-def s2t():
-    result = model.transcribe("output.wav", fp16=False, language='Spanish')    
-    return(result["text"].lower())
-
-# -_-_-_-_-_-_-_-_-_-_-_-_ Text to speech method -_-_-_-_-_-_-_-_-_-_-_-_
-def t2s(msg):
-    speech = gTTS(text = msg, lang = 'es')
-    speech.save('computerAudio.mp3')
-    pygame.mixer.init()
-    pygame.mixer.music.load("computerAudio.mp3")
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
-
-# -_-_-_-_-_-_-_-_-_-_-_-_ Get audio and apply s2t -_-_-_-_-_-_-_-_-_-_-_-_     
-def recordAudio(time):
-    getAudio(time)
-    data = s2t()
-    return data
-
-# -_-_-_-_-_-_-_-_-_-_-_-_  -_-_-_-_-_-_-_-_-_-_-_-_
-def capture_photo(file_capture, text):
-    camera.annotate_text = text
-    sleep(2)
-    camera.capture(file_capture)
-    print("\r\nImage Captured! \r\n")
-    
+def ledOn(pin, time):
+    GPIO.output(16, GPIO.HIGH)
+    sleep(n)
+    GPIO.output(16, GPIO.LOW)
